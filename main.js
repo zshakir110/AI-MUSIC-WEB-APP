aff2927fe869062585134755af9f76b74e2f0a99
zshakir110 <78383982+zshakir110@users.noreply.github.com>
2021-08-30 08:01:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup() { 
+    canvas = createCanvas(550, 550);
+    canvas.position(500,170);
+}
+
+function draw() {
+    background('#0b0026');
+      fill('#00FFFF');
+      stroke('#00FFFF');
+    }
