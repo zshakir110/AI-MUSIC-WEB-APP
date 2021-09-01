3e78c467a5565f97f389d9f75febaf662d252c36
zshakir110 <78383982+zshakir110@users.noreply.github.com>
2021-09-01 18:19:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 function setup() { 
     canvas = createCanvas(550, 550);
     canvas.position(500,170);
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded); 
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.lenght > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX +"leftWristY =" +leftWristY); 
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX +"rightWristY =" +rightWristY); 
+  }
 }
 
 function draw() {
